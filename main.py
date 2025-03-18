dfda1eb1c27bf24fb5303b71597abf633155740d
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-03-17 22:39:22 -0400

    Initial commit for coffee machine project.
    TODOs written based on the given requirements

---
+++ main.py
@@ -0,0 +1,54 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+# TODO 3: Print report showing the current resource values when user writes "report". (Water ml, Milk ml, Coffee g, Money $)
+def currentResources():
+    for key in resources:
+        print(key)
+        print(resources[key])
+
+currentResources()
+# TODO 1: Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
+order = input("What would you like? (espresso/latte/cappuccino): ").lower
+# TODO 2: Turn off the Coffee Machine by entering “off” to the prompt. (code should end)
+while True:
+    if order == "off":
+        print("Good bye!")
+        break
+    elif order == "report":
+        print(currentResources)
+    else:
+        print("Thank you!")
+
+# TODO 4: Check resources sufficient?
+# TODO 5: Process coins.
+# TODO 6: Check transaction successful?
+# TODO 7: Make Coffee
