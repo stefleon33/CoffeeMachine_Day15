086245146759f4150894d9f3d630dd1b2b166e15
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-04-08 15:56:12 -0400

    Final code for coffee machine. Function fixes

--- main.py
+++ main.py
@@ -30,9 +30,11 @@ resources = {
     "coffee": 100,
 }
 
+profit = 0
+
 # TODO 3: Print report showing the current resource values when user writes "report". (Water ml, Milk ml, Coffee g, Money $)
-"""Function to print the current resources in the machine with units"""
 def current_resources():
+    """Function to print the current resources in the machine with units"""
     for resource, amount in resources.items():
         if resource == "water":
             unit = "ml"
@@ -43,16 +45,46 @@ def current_resources():
         else:
             unit = ""
         print(f"{resource.capitalize()}: {amount}{unit}")
+    print(f"Money: ${profit}")
 
 # TODO 5: Process coins.
-def payment():
+def process_coins():
     """Function to collect payment from the customer"""
-    quarters = int(input("How many quarters? "))
-    dimes = int(input("How many dimes? "))
-    nickles = int(input("How many nickles? "))
-    pennies = int(input("How many pennies? "))
-    total = (quarters * 0.25) + (dimes * 0.10) + (nickles * 0.05) + (pennies * 0.01)
-    print(total)
+    print("Please insert coins.")
+    total = int(input("How many quarters? ")) * 0.25
+    total += int(input("How many dimes? ")) * 0.10
+    total += int(input("How many nickles? ")) * 0.05
+    total += int(input("How many pennies? ")) * 0.01
+    return total
+
+# TODO 6: Check transaction successful?
+def is_transaction_successful(money_paid, beverage_cost):
+    """Return True when the payment is accepted, or False if money is insufficient."""
+    if money_paid >= beverage_cost:
+        change = round(money_paid - beverage_cost, 2)
+        print(f"Here is ${change} in change.")
+        global profit
+        profit += beverage_cost
+        return True
+    else:
+        print("Sorry, that's not enough money. Money refunded.")
+        return False
+
+# TODO 4: Check resources sufficient?
+def is_resource_sufficient(order_ingredients):
+    """If the customer orders a drink, the machine needs to check if there are enough resources available."""
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry, there is not enough {item}.")
+            return False
+    return True
+
+# TODO 7: Make Coffee
+def make_coffee(drink_name, order_ingredients):
+    """Deduct the required ingredients from the resources"""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"here is your {drink_name} ☕️. Enjoy!")
 
 coffe_machine_on = True
 while coffe_machine_on is True:
@@ -67,9 +99,8 @@ while coffe_machine_on is True:
         current_resources()
         continue
     else:
-        print("Please insert coins.")
-        payment()
-
-# TODO 4: Check resources sufficient?
-# TODO 6: Check transaction successful?
-# TODO 7: Make Coffee
+        drink = MENU[order]
+        if is_resource_sufficient(drink["ingredients"]):
+            payment= process_coins()
+            if is_transaction_successful(payment, drink["cost"]):
+                make_coffee(order, drink["ingredients"])
